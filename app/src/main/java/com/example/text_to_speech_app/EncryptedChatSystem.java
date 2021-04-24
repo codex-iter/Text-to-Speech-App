faaf2ff537b52f7df37bb0d210ccf01538e922c6
Prakash Pathak <54182748+Prakashpathak984@users.noreply.github.com>
2021-04-24 23:18:44 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/text_to_speech_app/EncryptedChatSystem.java
@@ -0,0 +1,14 @@
+package com.example.text_to_speech_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EncryptedChatSystem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encrypted_chat_system);
+    }
+}
