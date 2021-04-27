6b275ecaa89287d8018ee020a829af7c4d0b0534
UtkarshShankar <53379005+UtkarshShankar@users.noreply.github.com>
2021-04-27 10:32:25 +0530

    Update to text to speech function

--- app/src/main/java/com/example/text_to_speech_app/TextToSpeech.java
+++ app/src/main/java/com/example/text_to_speech_app/TextToSpeech.java
@@ -40,6 +40,44 @@ public class TextToSpeech extends AppCompatActivity
 
             }
         });
+        
+        public void doSomething(View v)
+        {
+            switch (v.getId())
+            {
+                case R.id.speak:
+
+                    if(result==TextToSpeech.LANG_NOT_SUPPORTED||result==TextToSpeech.LANG_MISSING_DATA)
+                    {
+                        Toast.makeText(getApplicationContext(), "Feature not supported in your device", Toast.LENGTH_SHORT).show();
+
+                    }
+                    else
+                    {
+                        text = et.getText().toString();
+                        ttsobject.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+                    }
+                    break;
+
+                case R.id.stop:
+
+                    if(ttsobject!=null)
+                    {
+                        ttsobject.stop();
+                    }
+                    break;
+
+           }
+        }
+        @Override
+          protected void onDestroy() {
+        super.onDestroy();
+        if(ttsobject!=null)
+        {
+            ttsobject.stop();
+            ttsobject.shutdown();
+        }
+    }
           
           stop.setOnClickListener(new View.OnClickListener() {
             @Override
