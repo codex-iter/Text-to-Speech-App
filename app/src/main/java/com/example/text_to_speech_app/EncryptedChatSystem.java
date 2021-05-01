bb469523d0699147c5cca96db477c4b8ec998452
UtkarshShankar <53379005+UtkarshShankar@users.noreply.github.com>
2021-05-01 14:21:07 +0530

    Add files via upload

--- app/src/main/java/com/example/text_to_speech_app/EncryptedChatSystem.java
+++ app/src/main/java/com/example/text_to_speech_app/EncryptedChatSystem.java
@@ -1,7 +1,7 @@
 package com.example.text_to_speech_app;
 
 import android.Manifest;
-
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -9,17 +9,23 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Telephony;
-
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
 import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.security.MessageDigest;
+import java.util.Base64;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class EncryptedChatSystem extends AppCompatActivity {
 
@@ -32,6 +38,10 @@ public class EncryptedChatSystem extends AppCompatActivity {
     private Button bt_send;
     private Button bt_decrypt;
 
+    String AES="AES";
+    String string="";
+    String outputString="";
+    String tempEncrypted="";
 
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -77,6 +87,104 @@ public class EncryptedChatSystem extends AppCompatActivity {
         {
             requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE},3);
         }
+
+
+        bt_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M)
+                {
+                    if(checkSelfPermission(Manifest.permission.SEND_SMS)== PackageManager.PERMISSION_GRANTED) {
+                        if(et_message.length()>0 && et_phone.length()>0 && et_phone.length()<11 && et_password.length()!=0) {
+                            try {
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                    outputString = encrypt(et_message.getText().toString(), et_password.getText().toString());
+                                }
+                                tempEncrypted = outputString;
+//                        outputText.setText(outputString);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            sendSMS();
+
+                            et_message.setText("");
+                            et_phone.setText("");
+                            et_password.setText("");
+                            bt_send.setVisibility(View.GONE);
+                            bt_decrypt.setVisibility(View.VISIBLE);
+                        }
+                        else if(et_phone.length()==0)
+                        {
+                            Toast.makeText(getApplicationContext(), "Please enter a number", Toast.LENGTH_LONG).show();
+                            et_phone.requestFocus();
+                        }
+                        else if(et_message.length()==0)
+                        {
+                            Toast.makeText(getApplicationContext(), "Please write your message", Toast.LENGTH_LONG).show();
+                            et_message.requestFocus();
+                        }
+                        else if(et_password.length() == 0)
+                        {
+                            Toast.makeText(getApplicationContext(), "Password cannot be null", Toast.LENGTH_LONG).show();
+                            et_password.requestFocus();
+                        }
+
+                    }
+
+                    else
+                        requestPermissions(new String[]{Manifest.permission.SEND_SMS},1);
+
+                }
+
+
+
+
+            }
+        });
+
+
+
+
+
+    }
+
+
+  
+
+    private SecretKeySpec generateKey(String password) throws Exception{
+        final MessageDigest digest=MessageDigest.getInstance("SHA-256");
+        byte[] bytes=password.getBytes("UTF-8");
+        digest.update(bytes, 0,bytes.length);
+        byte[] key =digest.digest();
+        SecretKeySpec secretKeySpec=new SecretKeySpec(key, "AES");
+        return secretKeySpec;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String encrypt(String Data, String Password) throws Exception{
+        SecretKeySpec key =generateKey(Password);
+        Cipher c=Cipher.getInstance(AES);
+        c.init(Cipher.ENCRYPT_MODE,key);
+        byte[] encVal =c.doFinal(Data.getBytes());
+        String encryptedValue = Base64.getEncoder().encodeToString(encVal );
+        return encryptedValue;
+    }
+
+    private void sendSMS(){
+        String PhoneNo=et_phone.getText().toString().trim();
+        String SMS=tempEncrypted;
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(PhoneNo, null, SMS, null, null);
+            Toast.makeText(getApplicationContext(),"Message sent :)",Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"Message not sent :')",Toast.LENGTH_LONG).show();
+        }
+    }
 
 
 }
