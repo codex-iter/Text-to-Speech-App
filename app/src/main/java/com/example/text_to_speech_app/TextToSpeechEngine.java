c6171df408714d9a5ab02cd1fb2725e8c02c52f8
Prakash Pathak <54182748+Prakashpathak984@users.noreply.github.com>
2021-05-03 11:34:15 +0530

    added rate of speech functionality

--- app/src/main/java/com/example/text_to_speech_app/TextToSpeechEngine.java
+++ app/src/main/java/com/example/text_to_speech_app/TextToSpeechEngine.java
@@ -1,12 +1,16 @@
 package com.example.text_to_speech_app;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
@@ -19,11 +23,20 @@ public class TextToSpeechEngine extends AppCompatActivity {
     Button speak, stop;
     String text;
 
+    SeekBar speechRate;
+    TextView tv_rate;
+
+    float rate = 1.0f;
+    float step = 0.02f;
+    float min = 0.1f;
+
+
 
     public TextToSpeechEngine() {
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +46,32 @@ public class TextToSpeechEngine extends AppCompatActivity {
         speak = findViewById(R.id.bt_speak);
         stop = findViewById(R.id.bt_stop);
 
+        tv_rate = findViewById(R.id.tv_rate);
+
+        tv_rate.setText("45");
+//        tv_rate.setVisibility(View.VISIBLE);
+        speechRate = findViewById(R.id.sb_rate);
+        speechRate.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+
+                rate = min+(progress*step);
+                tts.setSpeechRate(rate);
+                tv_rate.setText(Integer.toString(progress));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
         tts =new TextToSpeech(TextToSpeechEngine.this, new TextToSpeech.OnInitListener() {
             @Override
@@ -40,6 +79,7 @@ public class TextToSpeechEngine extends AppCompatActivity {
 
                 if(i == TextToSpeech.SUCCESS){
                     result = tts.setLanguage(Locale.UK);
+                    tts.setSpeechRate(rate);
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "Feature not supported on your device", Toast.LENGTH_SHORT).show();
@@ -47,6 +87,8 @@ public class TextToSpeechEngine extends AppCompatActivity {
 
             }
         });
+
+
 
 
         speak.setOnClickListener(new View.OnClickListener() {
